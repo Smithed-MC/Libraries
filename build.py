8b60b44063cfb7aed7b79ee19a764a60d1277bf1
Ritik Shah <ritik10.shah@gmail.com>
2022-03-06 23:42:05 -0800

    feat: fixed build tool properly including versioning plugin and updated beet.yamls

---
+++ build.py
@@ -0,0 +1,33 @@
+from beet import Context, subproject
+from pathlib import Path
+
+import logging
+
+logger = logging.getLogger("build")
+logger.setLevel("INFO")
+
+
+def beet_default(ctx: Context):
+    for file in Path("libs").iterdir():
+        if file.is_dir():
+            try:
+                logger.info("Building Project %s", file)
+                
+                ctx.require(
+                    subproject(
+                        {
+                            "directory": str(file),
+                            "extend": ["beet.yaml"],
+                            "output": "../../dist",
+                            "pipeline": [
+                                "default.versioning",
+                                "beet.contrib.dundervar"
+                            ],
+                            "data_pack": {"zipped": True},
+                            "resource_pack": {"zipped": True},
+                        }
+                    )
+                )
+
+            except Exception as err:
+                logger.exception(err)
--- default/versioning.py
+++ default/versioning.py
@@ -3,7 +3,11 @@ from typing import Optional
 from beet.library.data_pack import Function, FunctionTag
 from beet.toolchain.context import Context
 
-version_part_names = ["major", "minor", "patch"]
+import logging
+
+logger = logging.getLogger("versioning")
+
+version_part_names: list[str] = ["major", "minor", "patch"]
 next_step_names = [*version_part_names[1:], None]
 step_pairs = zip(version_part_names, next_step_names)
 
@@ -23,39 +27,43 @@ def call_if_version_match(scoreholder, version_parts, path):
     return (
         "execute "
         + combine_parts(
-            f"if score {scoreholder}.{{name}} load.status matches {{part}} ", version_parts
+            f"if score {scoreholder}.{{name}} load.status matches {{part}} ",
+            version_parts,
         )
         + f"run function {path}"
     )
 
 
 def resolve(ctx: Context, namespace, version, scoreholder, version_parts):
-    name = f"{namespace}/calls/{version}/technical/resolve"
+    name = f"{namespace}:calls/{version}/technical/resolve"
 
     ctx.data[name] = Function(
-        f"schedule clear {namespace}/impl/{version}/technical/tick\n"
+        f"schedule clear {namespace}:impl/{version}/technical/tick\n"
         + call_if_version_match(
-            scoreholder, version_parts, f"{namespace}/impl/{version}/technical/load"
+            scoreholder, version_parts, f"{namespace}:impl/{version}/technical/load"
         )
     )
 
-    ctx.data[f"{namespace}/load/resolve"] = FunctionTag({"values": [name]})
+    ctx.data[f"{namespace}:load/resolve"] = FunctionTag({"values": [name]})
 
 
 def enumerate_step_name(namespace, version, step):
-    return f"{namespace}/calls/{version}/technical/enumerate/{step}"
+    return f"{namespace}:calls/{version}/technical/enumerate/{step}"
 
 
 def enumerate(ctx: Context, scoreholder, namespace, version):
-    name = f"{namespace}/calls/{version}/technical/enumerate"
+    name = f"{namespace}:calls/{version}/technical/enumerate"
+    step = version_part_names[0]
 
     ctx.data[name] = Function(
         f"scoreboard players reset {scoreholder}.set load.status\n"
-        + combine_part_names(f"scoreboard players add {scoreholder}.{{name}} load.status 0\n")
-        + f"function {enumerate_step_name(namespace, version_part_names[0], version)}"
+        + combine_part_names(
+            f"scoreboard players add {scoreholder}.{{name}} load.status 0\n"
+        )
+        + f"function {enumerate_step_name(namespace, version, step)}"
     )
 
-    ctx.data[f"{namespace}/load/enumerate"] = FunctionTag({"values": [name]})
+    ctx.data[f"{namespace}:load/enumerate"] = FunctionTag({"values": [name]})
 
 
 def enumerate_step(
@@ -89,11 +97,12 @@ def enumerate_step(
 
 
 def set_version(ctx: Context, namespace, scoreholder, version, version_parts):
-    name = f"{namespace}/calls/{version}/technical/enumerate/set_version"
+    name = f"{namespace}:calls/{version}/technical/enumerate/set_version"
 
     ctx.data[name] = Function(
         combine_parts(
-            f"scoreboard players set {scoreholder}.{{name}} load.status {{part}}", version_parts
+            f"scoreboard players set {scoreholder}.{{name}} load.status {{part}}",
+            version_parts,
         )
         + f"scoreboard players set {scoreholder}.set load.status 1"
     )
@@ -105,18 +114,22 @@ def replace_version(ctx: Context, namespace, version):
     # TODO: Potentially ctx.data.values() might implement multiple namespaces
     for container in ctx.data[namespace].values():
         for path in list(container):
-            if '__version__' not in path:
-                container[path.replace(f'__version__', version)] = container.pop(path)
+            if "__version__" not in path:
+                container[path.replace(f"__version__", version)] = container.pop(path)
 
 
-def generate_call(ctx, path: str, version: str, version_parts, namespace: str, scoreholder: str):
+def generate_call(
+    ctx: Context, path: str, version: str, version_parts, namespace: str, scoreholder: str
+):
     api_path = f"#{namespace}:{path.split(version)[1][1:]}"
     call_path = path.replace("impl", "calls")
 
-    print("  api:", api_path)
+    logger.info("api: %s", api_path)
 
     ctx.data[api_path] = FunctionTag({"values": [call_path]})
-    ctx.data[call_path] = Function(call_if_version_match(scoreholder, version_parts, path))
+    ctx.data[call_path] = Function(
+        call_if_version_match(scoreholder, version_parts, path)
+    )
 
 
 def generate_api_calls(ctx: Context, version, version_parts, namespace, scoreholder):
@@ -126,22 +139,41 @@ def generate_api_calls(ctx: Context, version, version_parts, namespace, scorehol
         if first_line.startswith("#") and "@public" in first_line:
             generate_call(ctx, path, version, version_parts, namespace, scoreholder)
 
+def resolve_advancements(ctx: Context, version, version_parts, namespace, scoreholder):
+    for path in ctx.data.advancements.match("impl"):
+        if (adv := ctx.data.advancements[path]).get("__public__", False):
+            adv["rewards"]["function"] = adv["rewards"]["function"].replace(
+                "__version__", version
+            )
+
+            generate_call(ctx, path, version, version_parts, namespace, scoreholder)
 
 def beet_default(ctx: Context):
     version = ctx.template.globals["version"] = f"v{ctx.project_version}"
-    scoreholder = ctx.meta["versioning"]["scoreholder"]
-    namespace = ctx.meta["versioning"]["namespace"]
-    version_parts = ctx.project_version.split(".")
+    scoreholder: str = ctx.meta["versioning"]["scoreholder"]
+    namespace: str = ctx.meta["versioning"]["namespace"]
+    version_parts: list[str] = ctx.project_version.split(".")
 
     yield
 
     replace_version(ctx, namespace, version)
     generate_api_calls(ctx, version, version_parts, namespace, scoreholder)
+    resolve_advancements(ctx, version, version_parts, namespace, scoreholder)
 
+    # TODO: Generate LL completely + other load tags
     resolve(ctx, namespace, version, scoreholder, version_parts)
     enumerate(ctx, scoreholder, namespace, version)
 
     set_version_func = set_version(ctx, namespace, scoreholder, version, version_parts)
 
     for (step, next_step), part in zip(step_pairs, version_parts):
-        enumerate_step(ctx, scoreholder, namespace, version, step, next_step, part, set_version_func)
+        enumerate_step(
+            ctx,
+            scoreholder,
+            namespace,
+            version,
+            step,
+            next_step,
+            part,
+            set_version_func,
+        )
