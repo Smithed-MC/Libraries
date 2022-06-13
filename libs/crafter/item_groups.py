0fee163ed87a363f377c21eace457622d171884b
TheNuclearNexus <thenuclearnexus@gmail.com>
2022-06-13 16:25:19 -0400

    **feat**: added heavy workbench to decoration tab

---
+++ libs/crafter/item_groups.py
@@ -0,0 +1,8 @@
+from beet import Context, JsonFile
+
+class ItemGroups(JsonFile):
+    scope = ('item_groups',)
+    extension = '.json'
+
+def beet_default(ctx: Context):
+    ctx.data.extend_namespace.append(ItemGroups)
