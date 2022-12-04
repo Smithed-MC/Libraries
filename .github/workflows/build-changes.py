a9286d352b59485d61f2585f05d79459c3b845d8
RitikShah <ritik10.shah@gmail.com>
2022-12-04 01:32:09 -0800

    chore: fix issue

---
+++ .github/workflows/build-changes.py
@@ -0,0 +1,52 @@
+import subprocess
+from pathlib import Path
+
+import yaml
+
+# Initialize the list of packs to build
+broadcast = []
+
+# Get a list of all the packs in the "smithed_libraries/packs" directory
+for pack in Path("smithed_libraries/packs").glob("*"):
+    # Get the "beet.yaml" file for the pack
+    beet_yaml_path = pack / "beet.yaml"
+
+    # Parse the "beet.yaml" file for the current commit using the pyyaml library
+    with open(beet_yaml_path) as f:
+        beet_yaml = yaml.safe_load(f)
+
+    # Get the version number of the pack from the "beet.yaml" file for the current commit
+    pack_version = beet_yaml["version"]
+
+    # Parse the "beet.yaml" file for the previous commit using the pyyaml library
+    old_beet_yaml = yaml.safe_load(
+        subprocess.check_output(
+            ["git", "show", "HEAD~1:" + str(beet_yaml_path)]
+        ).decode()
+    )
+
+    # Get the version number of the pack from the "beet.yaml" file for the previous commit
+    old_pack_version = old_beet_yaml["version"]
+
+    # Check if the pack's version number has changed
+    if pack_version != old_pack_version:
+        # Add the pack to the list of packs to build
+        broadcast.append(f"-s broadcast[] = {pack}")
+
+# Build the packs (if there are any to build)
+if broadcast:
+    subprocess.run(
+        [
+            "poetry",
+            "run",
+            "beet",
+            "--log",
+            "INFO",
+            "-p",
+            "beet-release.yaml",
+            *broadcast,
+        ]
+    )
+else:
+    # Raise an error to stop the rest of the steps
+    raise SystemExit("No packs to build")
