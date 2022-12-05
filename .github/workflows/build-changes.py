6db7aa859d445469dc31a86364f38ef58299d6d9
RitikShah <ritik10.shah@gmail.com>
2022-12-04 20:23:17 -0800

    chore: load manifest properly

--- .github/workflows/build-changes.py
+++ .github/workflows/build-changes.py
@@ -1,40 +1,34 @@
+import json
 import os
 import subprocess
 from pathlib import Path
 
 import yaml
 
+# Load manifest
+manifest = json.loads(Path("dist/manifest.json").read_text())
+last_commit = manifest["last_commit"]
+
 # Initialize the list of packs to build
 broadcast = []
 
 # Get a list of all the packs in the "smithed_libraries/packs" directory
 for pack in Path("smithed_libraries/packs").glob("*"):
-    # Get the "beet.yaml" file for the pack
+    # Gather beet yaml path and parse it
     beet_yaml_path = pack / "beet.yaml"
+    beet_yaml = yaml.safe_load(beet_yaml_path.read_text())
 
-    # Parse the "beet.yaml" file for the current commit using the pyyaml library
-    with open(beet_yaml_path) as f:
-        beet_yaml = yaml.safe_load(f)
-
-    # Get the version number of the pack from the "beet.yaml" file for the current commit
-    pack_version = beet_yaml["version"]
-    
-    last_commit = beet_yaml["last_commit"]
-
-    # Parse the "beet.yaml" file for the previous commit using the pyyaml library
+    # Parse the "beet.yaml" file for the previous commit
     old_beet_yaml = yaml.safe_load(
         subprocess.check_output(
             ["git", "show", f"{last_commit}:" + str(beet_yaml_path)]
         ).decode()
     )
 
-    # Get the version number of the pack from the "beet.yaml" file for the previous commit
-    old_pack_version = old_beet_yaml["version"]
-
-    # Check if the pack's version number has changed
-    if pack_version != old_pack_version:
-        # Add the pack to the list of packs to build
+    # Check if the pack's version number has changed and add pack to list of builds
+    if beet_yaml["version"] != old_beet_yaml["version"]:
         broadcast.append(f"-s pipeline[0].broadcast[] = {pack}")
+
 
 # Build the packs (if there are any to build)
 os.environ["GITHUB_OUTPUT"] = f"packs="
