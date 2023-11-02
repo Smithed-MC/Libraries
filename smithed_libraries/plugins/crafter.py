02b544a1e00454478000bc58a939ab363dd8bd4a
RitikShah <ritik10.shah@gmail.com>
2023-11-01 23:47:03 -0400

    fix: use updated code from @fizzy

--- smithed_libraries/plugins/crafter.py
+++ smithed_libraries/plugins/crafter.py
@@ -1,21 +1,11 @@
-import requests
 from beet import Context, ItemTag
 
 
-def cache_item_list(ctx: Context, cache_identifier: str):
-    url = f"https://raw.githubusercontent.com/misode/mcmeta/{ctx.minecraft_version}-registries/item/data.json"
-    r = requests.get(url)
-    if not r.ok:
-        raise Exception(f"Failed to get {url}")
-    data = r.json()
-    ctx.cache.json[cache_identifier] = data
-
-
 def beet_default(ctx: Context):
-    cache_identifier = f"item_list@{ctx.minecraft_version}"
-    if cache_identifier not in ctx.cache.json:
-        cache_item_list(ctx, cache_identifier)
+    cache = ctx.cache["smithed_crafter"]
 
-    ctx.data.item_tags["smithed.crafter:all"] = ItemTag(
-        {"values": ctx.cache.json[cache_identifier]}
-    )
+    all_items_url = f"https://raw.githubusercontent.com/misode/mcmeta/{ctx.minecraft_version}-registries/item/data.json"
+    all_items = ItemTag(source_path=cache.download(all_items_url))
+    all_items.data = {"values": all_items.data}
+
+    ctx.generate("smithed.crafter:all", all_items)
